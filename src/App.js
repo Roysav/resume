c9ac76797f09b96e2e7ab2b673cca74795385f03
Roysav <roysav123@gmail.com>
2023-05-02 14:18:17 +0300

    added firebase hosting workflow and a link to the repo in the webpage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import Skills from "./Skills";
 function App() {
     return (
         <div className="flex flex-col items-center justify-items-center w-screen h-screen" dir="rtl">
-            <p className="text-xl noprint">הקו האפור מסביב זה גבולות הדף, כשמדפיסים לpdf לא רואים אותם</p>
+            <p className="text-xl noprint">
+                הקו האפור מסביב זה גבולות הדף, כשמדפיסים לpdf לא רואים אותו.
+            </p>
+
+            <p className="noprint">תערכו כאן:
+                <a href="https://github.com/Roysav/resume" className="noprint">github</a>
+            </p>
             <div className="grid place-items-center w-[210mm] px-8 py-16 border border-noprint">
                 <main className="flex flex-row-reverse" >
                     <div className="w-3/4 flex flex-col">
